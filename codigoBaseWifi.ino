8c65f392ec3a2e75d1e53fd8a4f6f1245297513f
darkvictor13 <51101723+darkvictor13@users.noreply.github.com>
2019-10-28 11:29:28 -0300

    Add files via upload

---
+++ codigoBaseWifi.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+#define LED D4 //Porta D4 - Saida para o lED
+
+String command;             //String que guarda o comando
+
+const char* ssid = "Esp8266WemosD1MiniPro"; //Nome da rede
+
+ESP8266WebServer server(80);
+
+void setup() {
+ 
+  Serial.begin(115200);
+  
+// Connecting WiFi
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");//Imprime no serial o ip do servidor - Entrar nesse ip pelo navegador
+  Serial.println(myIP);
+ 
+ // Starting WEB-server 
+     server.on ( "/", HTTP_handleRoot );
+     server.onNotFound ( HTTP_handleRoot );
+     server.begin();  
+
+
+   //PReparando as portas para Usar com o led
+   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(LED_BUILTIN, LOW);
+   pinMode(LED, OUTPUT);
+   digitalWrite(LED, LOW); 
+}
+
+
+void loop() {
+    server.handleClient();
+    
+      command = server.arg("LED1");
+      if (command == "L") 
+        digitalWrite(LED, HIGH); 
+      if (command == "D")
+        digitalWrite(LED, LOW);
+}
+
+void HTTP_handleRoot(void) {
+  
+if( server.hasArg("LED1") ){
+       Serial.println(server.arg("LED1"));
+  }
+  server.send ( 200, "text/html",  "<html> <form align=\"center\" method=\"SET\"> <input type=\"radio\" name=\"LED1\" value=\"L\"> Ligar LED <br> <input type=\"radio\" name=\"LED1\" value=\"D\"> Desligar LED<br>    <input type=\"submit\" value=\"Submit\" /> </form> </html>"  );
+  delay(1);
+}
